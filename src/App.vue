db3eae996ce3e3b57cc7ee29be35d4eacb056e13
Pietro907 <pietrop.puri@gmail.com>
2023-09-29 02:52:35 +0200

    add comment

--- src/App.vue
+++ src/App.vue
@@ -12,17 +12,23 @@ export default {
 
   },
   methods: {
-    filterFilms(){
+
+    //Metodo per filtrare i film
+    filterFilms() {
 
       const movie_url = this.state.base_url + this.state.search_site;
-   
+
+      //Alla varibile result gli assegno la funzione di filtro con il parametro da filtrare
       let result = this.state.fecthData(movie_url);
 
       console.log(result);
-      
+
     },
-    /* changeToFlag(){
-      const lang = this.state.base_url + 'original_language&query=' + this.state.search_site;
+
+
+
+    /* changeToFlag(){          
+      const lang = this.state.base_url + 'original_language&query=' + this.state.search_site;     //Una prova e forse ci sono vicino??????
       let resultLang = this.state.fecthData(lang);
       console.log(resultLang);
       if (lang == it) {
@@ -39,26 +45,36 @@ export default {
 </script>
 
 <template>
-
+  <!-- Una prova per vedere lo state collegato -->
   <h1>{{ state.movie }}</h1>
 
+  <!-- Input qui faccio la ricerca con nome per cercare un film  -->
+  <!-- Posso inviare la richiesta premendo il tasto enter -->
   <input @keyup.enter="filterFilms()" type="search" id="research_site" placeholder="Search the site…" v-model="state.search_site">
+
+  <!-- Btn per inviare la richiesta di filtraggio film -->
   <button @click="filterFilms()">Click me</button>
-  
 
+  <!-- Lista non ordinata in loop per creare una lista per film trovato -->
   <ul v-for="film in this.state.films">
-    <li>{{ film.title }}</li>
-    <li>{{ film.original_title }}</li>
-    <li>{{ film.original_language }}</li>
-    <li>{{ Math.floor(film.vote_average)}}</li>
-  </ul>
 
+    <!-- TItolo -->
+    <li>{{ film.title }}</li>
+
+    <!-- Titolo originale -->
+    <li>{{ film.original_title }}</li>
+
+    <!-- Language -->
+    <li>{{ film.original_language }}</li>
+
+    <!-- Votazione del film arrotondata -->
+    <li>{{ Math.floor(film.vote_average) }}</li>
+
+  </ul>
 </template>
 
 <style scoped>
-
-.body{
+.body {
   margin: 0;
 }
-
 </style>
--- src/state.js
+++ src/state.js
@@ -12,9 +12,12 @@ export const state = reactive({
 
     base_url: 'https://api.themoviedb.org/3/search/movie?api_key=c141324a0c6f42eee2d14ae8c38900a5&query=',
 
+    //Fa una chiamata usando una parametro al base_url e restituisce i dati contenuti nel JSON
     fecthData(url){
 
-
+        //Questo si installa prima di iniziare il progetto
+        //Con il metodo .get effettuo una chiamata e con il parametro gli dico
+        //che questo metodo vuole un parametro per funzione e poterla riciclare ^_^
         axios.get(url)
         .then(res => {
             console.log(res + ' filterFilms()');
@@ -23,12 +26,19 @@ export const state = reactive({
             this.films = res.data.results;
             console.log(this.films);
         })
-        .catch(error => {
+        .catch(error => {                //Non lo comprendo bene -.-'
             console.error(error);
         })
     },
+
+
+
+
+
+
     /* langToFlag(language, el) {
-        let it = this.film.original_language
+        let it = this.film.original_language                  //Una prova ma la devo vedere bene, non so se è giusta
+                                                              //?????
         
         if (container.contains('open')) {
           container.remove('open');
