df6fa72ccd3b5ec550a17b59c7d291eaa7f7d8bd
Pietro907 <pietrop.puri@gmail.com>
2023-09-30 18:12:12 +0200

    add research for multi call (movie and tv) on screen

--- src/state.js
+++ src/state.js
@@ -7,20 +7,28 @@ export const state = reactive({
     search_site: '',
 
     //variabile alla quale assegneremo il percorso iniziale di call al sito (meglio usare un [] invece di '')
-    films: [],
+    films: [], //cambiare nome? films in realtà è l'array che si riempe ad ogni chiamata e al refresh dell input si svuota (non della pagina)
 
-    //Se lo scompongo in api_key: and query: mi aiuta se un giorno devo modificare
-    base_url: 'https://api.themoviedb.org/3/search/movie?api_key=c141324a0c6f42eee2d14ae8c38900a5&query=',
+    //variabile alla quale assegneremo il percorso iniziale di call al sito (meglio usare un [] invece di '')
+    seriesTv: [],
+
+    //Api key generata da the movie db
+    apy_key: '&api_key=' + '1a17221b0678ba34280852355cc8d4b7',
+
+    //Se lo scompongo in api_key: and query: mi aiuta se un giorno devo modificare (se ultilizzo movie=film, tv=serietv, multi=multiricerca)
+    base_url: 'https://api.themoviedb.org/3/search/multi?&api_key=1a17221b0678ba34280852355cc8d4b7&query=',
+
+    seriesTv_url: 'https://api.themoviedb.org/3/search/serietv?&api_key=1a17221b0678ba34280852355cc8d4b7&query=',
 
     //Fa una chiamata usando una parametro al base_url e restituisce i dati contenuti nel JSON
     fecthData(url){
 
         //Questo si installa prima di iniziare il progetto
-        //Con il metodo .get effettuo una chiamata e con il parametro gli dico
+        //Con il metodo .get effettuo una chiamata e con il parametro gli dico                        //funzione riutilizzabile dal search serieTV???
         //che questo metodo vuole un parametro per funzione e poterla riciclare ^_^
         axios.get(url)
         .then(res => {
-            console.log(res + ' filterFilms()');
+            console.log(res + ' fecthData()');
 
             //percorso abbrevviato per accedere ai dati del sito
             this.films = res.data.results;
