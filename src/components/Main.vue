6d54bf114aa96e324bc9e99a79d6e237c9866d7b
Pietro907 <pietrop.puri@gmail.com>
2023-10-02 01:56:38 +0200

    add style Main and caCardMain and added hover on card

---
+++ src/components/Main.vue
@@ -0,0 +1,65 @@
+<script>
+import { state } from '../state.js'
+import CardMain from './CardMain.vue';
+export default {
+    name: 'Main',
+
+    components: {
+        CardMain,
+    },
+    data() {
+        return {
+            state,
+        }
+    }
+}
+</script>
+
+
+
+<template>
+    <div class="container bg_dark_light">
+        <div class="row flex_wrap bg_dark">
+
+
+            <CardMain />
+
+
+        </div>
+    </div>
+</template>
+
+
+
+<style scoped>
+.container {
+    height: calc(100vh - 90px);
+    display: flex;
+    flex-direction: row;
+
+}
+
+.row {
+    padding: 2rem;
+    width: 90%;
+    margin: 2rem auto;
+    overflow-y: auto;
+}
+
+.flex_wrap {
+    display: flex;
+    flex-direction: row;
+    flex-wrap: wrap;
+    justify-content: center;
+    align-items: center;
+    gap: 1rem;
+}
+
+.bg_dark_light {
+    background-color: rgba(0, 0, 0, 0.851);
+}
+
+.bg_dark {
+    background-color: black;
+}
+</style>
